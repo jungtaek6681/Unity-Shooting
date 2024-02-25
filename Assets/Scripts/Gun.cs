a16159830a1db445ef594f704cd66eede6469ba2
jungtaek kim <kgajungtaek@gmail.com>
2024-02-26 00:39:33 +0900

    feat : 파티클 시스템
    씬 안에 다수의 작은 입자을 생성하고 애니메이션하여 액체, 구름 및 불꽃 같은 유체 엔티티를 시뮬레이션함
    유니티에서 간단한 효과를 만들기 위해 사용

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     [SerializeField] Transform muzzlePoint;
+    [SerializeField] ParticleSystem muzzleEffect;
+    [SerializeField] ParticleSystem hitEffectPrefab;
     [SerializeField] float maxDistance;
     [SerializeField] int damage;
     [SerializeField] float power;
@@ -12,6 +14,7 @@ public class Gun : MonoBehaviour
         if (Physics.Raycast(muzzlePoint.position, muzzlePoint.forward, out RaycastHit hitInfo, maxDistance))
         {
             Debug.DrawRay(muzzlePoint.position, muzzlePoint.forward * hitInfo.distance, Color.yellow, 0.1f);
+            muzzleEffect.Play();
 
             IDamagable damagable = hitInfo.collider.GetComponent<IDamagable>();
             if (damagable != null)
@@ -24,6 +27,9 @@ public class Gun : MonoBehaviour
             {
                 rigidbody.AddForceAtPosition(muzzlePoint.forward * power, hitInfo.point, ForceMode.Impulse);
             }
+
+            ParticleSystem effect = Instantiate(hitEffectPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+            effect.transform.parent = hitInfo.transform;
         }
         else
         {
