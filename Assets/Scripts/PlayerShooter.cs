8c0ca108cb12b2386dfff55aa248d9e26425615e
jungtaek kim <kgajungtaek@gmail.com>
2024-02-25 23:30:09 +0900

    feat : 애니메이션 리깅(IK)
    애니메이션 리깅을 이용하여 유니티 역운동학(IK) 구현
    역운동학은 자식의 위치&회전에 따라 부모의 위치&회전을 구하는 방식

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -1,9 +1,16 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 using UnityEngine.InputSystem;
 
 public class PlayerShooter : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] Rig aimRig;
+    [SerializeField] Rig weaponHolderRig;
+    [SerializeField] float reloadTime;
+
+    private bool reloading;
 
     private void Fire()
     {
@@ -12,16 +19,36 @@ public class PlayerShooter : MonoBehaviour
 
     private void Reload()
     {
+        StartCoroutine(ReloadRoutine());
+    }
+
+    IEnumerator ReloadRoutine()
+    {
         animator.SetTrigger("Reload");
+        reloading = true;
+        aimRig.weight = 0f;
+        weaponHolderRig.weight = 0f;
+
+        yield return new WaitForSeconds(reloadTime);
+
+        reloading = false;
+        aimRig.weight = 1f;
+        weaponHolderRig.weight = 1f;
     }
 
     private void OnFire(InputValue value)
     {
+        if (reloading)
+            return;
+
         Fire();
     }
 
     private void OnReload(InputValue value)
     {
+        if (reloading)
+            return;
+
         Reload();
     }
 }
--- Assets/Scripts/TPSCameraController.cs
+++ Assets/Scripts/TPSCameraController.cs
@@ -5,6 +5,8 @@ public class TPSCameraController : MonoBehaviour
 {
     [SerializeField] Transform cameraRoot;
     [SerializeField] private float mouseSensitivity;
+    [SerializeField] Transform lookTarget;
+    [SerializeField] float lookDistance;
 
     private Vector2 lookDelta;
     private float xRotation;
@@ -31,6 +33,8 @@ public class TPSCameraController : MonoBehaviour
         cameraRoot.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
         transform.Rotate(Vector3.up * lookDelta.x * mouseSensitivity * Time.deltaTime);
+
+        lookTarget.position = Camera.main.transform.position + Camera.main.transform.forward * lookDistance;
     }
 
     private void OnLook(InputValue value)
